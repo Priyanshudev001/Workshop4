887f296adf03d972b74126c72e99d042c44f4dee
Priyanshudev001 <sanjaydeo099@gmail.com>
2025-11-18 12:33:01 +0545

    again added task

---
+++ Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Task2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Create object of Calculator
+        Calculator calc = new Calculator();
+
+        // Call void method
+        calc.PrintWelcome();
+
+        // Call Add method
+        int addition = calc.Add(111, 222);
+        Console.WriteLine("Addition Result: " + addition);
+
+        // Call Multiply method (both parameters)
+        int multiply1 = calc.Multiply(55, 80);
+        Console.WriteLine("Multiplication (55 * 80): " + multiply1);
+
+        // Call Multiply method (optional parameter)
+        int multiply2 = calc.Multiply(2);  // num2 uses default = 1
+        Console.WriteLine("Multiplication (2 * default): " + multiply2);
+
+        Console.ReadLine();
+    }
+}
